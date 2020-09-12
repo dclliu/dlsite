51ca444247ae74ed2b20b332630f9f264e15ba75
Daniel Liu <danielliu@daniels-mbp-4.lan>
2020-09-12 16:06:07 -0400

    Added LILY project

--- src/constants/pubConstants.js
+++ src/constants/pubConstants.js
@@ -1,5 +1,13 @@
 export default [
   {
+    title: "(In progress) NLP Table Summarization",
+    imageUrl:
+      'https://i.imgur.com/AQSKxJf.png',
+    moreInfo: true,
+    text: "Currently working on state-of-the-art NLP models for table summarization with the LILY lab at Yale. Publication expected in November of this year.",
+    linkUrl: "https://yale-lily.github.io/",
+  },
+  {
     title: "iProteinDB, published 2019 in G3",
     imageUrl:
       'https://i.imgur.com/3fldVpc.png',
@@ -7,4 +15,5 @@ export default [
     text: "I did data analysis and make figures using a new database of Drosophila post-translational modifications.",
     linkUrl: "https://www.g3journal.org/content/9/1/1",
   },
+  
 ];
